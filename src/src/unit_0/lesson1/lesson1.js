0f753b9623b0b1141d9c2fbe5c4a2170e962262c
123lethabomosoathupa <lethabo.mosoathupa8@gmail.com>
2025-10-08 14:37:28 +0200

    first commit

---
+++ src/src/unit_0/lesson1/lesson1.js
@@ -0,0 +1,10 @@
+"use strict";
+3 + 3;
+3 / 0;
+console.log("Hello Universe!");
+let name = "Lethabo Mosoathupa";
+console.log(name);
+console.log(name);
+class Goat {eat(foodType) {console.log(`I love eating ${foodType}`); }}
+let billy = new Goat();
+billy.eat("tin cans");
