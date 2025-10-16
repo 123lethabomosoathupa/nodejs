ab186fb415bc47a7efc30da25be10d0b3815ce9a
123lethabomosoathupa <lethabo.mosoathupa8@gmail.com>
2025-10-16 16:01:48 +0200

    update

---
+++ Classroom/unit 2/lesson 11/controllers/errorController.js
@@ -0,0 +1,22 @@
+const httpStatus = require("http-status-codes");
+
+// Log errors to console
+exports.logErrors = (error, req, res, next) => {
+  console.error(error.stack);
+  next(error);
+};
+
+// Respond when a route isn't found
+exports.respondNoResourceFound = (req, res) => {
+  let errorCode = httpStatus.NOT_FOUND;
+  res.status(errorCode);
+  res.send(`${errorCode} | The page does not exist!`);
+};
+
+// Respond when a server error occurs
+exports.respondInternalError = (error, req, res, next) => {
+  let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
+  console.log(`ERROR occurred: ${error.stack}`);
+  res.status(errorCode);
+  res.send(`${errorCode} | Sorry, our application is experiencing a problem!`);
+};
--- Classroom/unit 2/lesson 11/main.js
+++ Classroom/unit 2/lesson 11/main.js
@@ -1,7 +1,14 @@
 const express = require("express");
- const app = express();
- const homeController = require("./controllers/homeController");
- const layouts = require("express-ejs-layouts");
+const app = express();
+const homeController = require("./controllers/homeController");
+const layouts = require("express-ejs-layouts");
+const errorController = require("./controllers/errorController");
+
+// Middleware order matters:
+app.use(errorController.logErrors);
+app.use(errorController.respondNoResourceFound);
+app.use(errorController.respondInternalError);
+app.use(express.static("public"));
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
@@ -27,5 +34,5 @@ app.post("/", homeController.sendPost);
 app.get("/name/:myName", homeController.respondWithName);
 
 app.listen(app.get("port"), () => {
-    console.log(`Server running at http://localhost:${app.get("port")}`);
+  console.log(`Server running at http://localhost:${app.get("port")}`);
 });
